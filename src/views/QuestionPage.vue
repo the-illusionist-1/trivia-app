82ed7ddeeac85b3ae3ee1cde41bf6dfae3a5b67b
Andres Flores <and05flo@proton.me>
2025-10-25 11:41:45 -0500

    Diffifculty added

--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -3,6 +3,7 @@ import {onMounted, ref} from "vue";
 import useAPI from '@/composables/useAPI'
 import {useRoute} from "vue-router";
 import BaseTitle from "@/components/BaseTitle.vue";
+import DifficultyChip from "@/components/DifficultyChip.vue";
 
 const api = useAPI()
 const question = ref(null)
@@ -52,6 +53,7 @@ const shuffle = (array) => {
            class="bg-green-500 flex items-center justify-center text-4xl rounded-lg text-white py-10 px-2">
       </div>
     </div> 
+    <DifficultyChip :difficulty="question.difficulty"></DifficultyChip>
   </div>
   <div v-else class="">
     Loading...
